8af7ff4dd8fb71760218e4a44874a49bbd345c43
Aarya Shinde <aaryashinde2020@gmail.com>
2025-03-21 08:58:58 +0530

    Problem-2

---
+++ addition_two_no.py
@@ -0,0 +1,8 @@
+# 2. Take two numbers as input and print their sum.
+
+num1 = int(input("Enter number you want to add: "));
+num2 = int(input("Enter number you want to add: "));
+
+sum = num1 + num2;
+
+print("The  total is ", sum);
