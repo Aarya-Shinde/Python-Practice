a54b73fb5da6a02e13ed1810266442c9dbd45137
Aarya Shinde <aaryashinde2020@gmail.com>
2025-03-23 11:29:11 +0530

    Problem -3

---
+++ swap_numbers.py
@@ -0,0 +1,19 @@
+# 3. Swap two numbers without using a third variable.  
+
+num1 = int(input("Enter number: "))
+num2 = int(input("Enter number: "))
+
+# Swapping numbers using arithmetic operators
+num1 = num1 + num2 
+# 30 = 10 +20
+print(num1)
+
+num2 = num1 - num2
+#10 = 30-20
+print(num2)
+
+num1 = num1 - num2
+# 20 = 30-10
+print(num1)
+
+print(num2)
